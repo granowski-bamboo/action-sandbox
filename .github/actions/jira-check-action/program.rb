3c9db0f42a303ac002a99b16777432f8f628106c
Derrick Granowski <dgranowski@hbu.co>
2022-07-13 16:20:30 -0600

    getting more fancy, reading the event json

--- .github/actions/jira-check-action/program.rb
+++ .github/actions/jira-check-action/program.rb
@@ -1,7 +1,16 @@
 #!ruby
 
-p "what we got here..."
-
 # for debugging, drop the environment vars of the ruby process so we can see what to expect
-ENV.each { |k,v| p k + ": " + v }
+$stdout.printf("--- ENVIRONMENT ---\n")
+ENV.each { |k,v| $stdout.printf("#{k}=#{v}\n") }
+$stdout.printf("-------------------\n")
 
+require 'uri'
+require 'net/http'
+
+require 'json'
+require 'yaml'
+
+event_data = JSON.parse(ENV['GITHUB_EVENT_PATH'])
+
+$stdout.printf(event_data.to_yaml)
